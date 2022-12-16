51b18710ea9c7a55e3db92e642b3a9dbbabb6db1
Gencisama <39381217+Gencisama@users.noreply.github.com>
2022-12-16 23:33:54 +0100

    Add files via upload

---
+++ coffee.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+
+st.text("My coffeemachine :)")
+
+st.image("https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/best-espresso-machines-1587116685.jpg?crop=0.401xw:0.801xh;0.289xw,0.0977xh&resize=640:*")
